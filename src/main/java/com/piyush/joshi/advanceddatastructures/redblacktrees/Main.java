c98efb0225c48ac7f0aa7bb6ec3fb0df53e2b641
spiderbot <joshipiyush217@gmail.com>
2024-05-16 13:44:15 +0530

    init

---
+++ src/main/java/com/piyush/joshi/advanceddatastructures/redblacktrees/Main.java
@@ -0,0 +1,7 @@
+package com.piyush.joshi.advanceddatastructures.redblacktrees;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
